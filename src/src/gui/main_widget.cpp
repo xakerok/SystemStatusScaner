312b1a36f7fd038be485287f1ebf7435dd7db0ed
JrLBo <boklag.jr.l@gmail.com>
2013-11-25 08:24:01 +0200

    Changed the size Columns QTreeWidget

--- src/src/gui/main_widget.cpp
+++ src/src/gui/main_widget.cpp
@@ -18,6 +18,20 @@ CMainWindowWidget::CMainWindowWidget(QWidget *parent) :
    m_pDataTimer->setInterval(1000);
    Q_ASSERT(connect(m_pDataTimer,SIGNAL(timeout()),this,SLOT(GetCurrDataValue())));
    m_pDataTimer->start();
+
+   //TODO: del following code
+   /*QTreeWidgetItem *item = new QTreeWidgetItem(ui.treeWidget);
+   item->setText( 0, "50000" );
+   item->setText( 1, "\"08:06:04.678\"" );
+   item->setText( 2, "100" );
+   item->setText( 3, "100" );*/
+   //end
+
+   ui.treeWidget->setColumnWidth( 0, 134 );
+   ui.treeWidget->setColumnWidth( 1, 175 );
+   ui.treeWidget->setColumnWidth( 2, 75 );
+   ui.treeWidget->setColumnWidth( 3, 75 );
+	//TODO" fixed size? It's possible?
 }
 
 CMainWindowWidget::~CMainWindowWidget()
